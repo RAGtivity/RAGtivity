96eecb37f90d3b5b8d790c636259d184aadee403
Vincent <verillytmt@gmail.com>
2025-12-25 21:19:39 +1100

    Turned off react-in-jsx-scope rule for ESLint

--- website/eslint.config.js
+++ website/eslint.config.js
@@ -3,7 +3,16 @@ import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
+
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   pluginReact.configs.flat.recommended,
+  { 
+    files: ["**/*.{js,mjs,cjs,jsx}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"], 
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "react/react-in-jsx-scope": "off"
+    } 
+  },
 ]);
