0a1b81d88eb0927762a4e542df020e83ea3a2512
xaveryh <verillytmt@gmail.com>
2026-01-25 00:53:37 +1100

    Resolving lint issue -> process is undefined

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,22 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+  /* eslint-env node */
 
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd())
+  import { defineConfig, loadEnv } from 'vite'
+  import react from '@vitejs/plugin-react'
+  import tailwindcss from '@tailwindcss/vite'
 
-  return {
-    plugins: [
-      react(),
-      tailwindcss(),
-    ],
-    server: {
-      host: "0.0.0.0",
-      allowedHosts: env.SERVER_DOMAIN ? [env.SERVER_DOMAIN] : undefined,
-      port: 5173
+  // https://vite.dev/config/
+  export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd())
+
+    return {
+      plugins: [
+        react(),
+        tailwindcss(),
+      ],
+      server: {
+        host: "0.0.0.0",
+        allowedHosts: env.SERVER_DOMAIN ? [env.SERVER_DOMAIN] : undefined,
+        port: 5173
+      }
     }
-  }
-})
+  })
