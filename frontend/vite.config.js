a688e1b33ba9a440368416937e1927d38c67a2e1
xaveryh <verillytmt@gmail.com>
2026-01-25 00:47:20 +1100

    Allowed deployment server domain name to the allowedHosts list on vite.config.js. Did this through env variable

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  server: {
-    host: "0.0.0.0",
-    port: 5173
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    server: {
+      host: "0.0.0.0",
+      allowedHosts: env.SERVER_DOMAIN ? [env.SERVER_DOMAIN] : undefined,
+      port: 5173
+    }
   }
 })
