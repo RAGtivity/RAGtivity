2942a1e3b09c69a52b1c300121942598d73e48f4
xaveryh <verillytmt@gmail.com>
2026-02-08 01:25:24 +1100

    Allowed CORS for FRONTEND_URL env variable -> for deployment

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ const s3client = new S3Client({
 const S3_BUCKET_NAME = "ragtivity"
 
 app.use(cors({ 
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true 
 }));
 app.use(fileUpload()); 
